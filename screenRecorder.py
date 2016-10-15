a881e3c074cebaedf7760a0cf14b13f76fd86395
Sean Trehy <PaleTurtle@users.noreply.github.com>
2016-10-14 18:59:50 -0700

    Updated file, all parts put together

--- screenRecorder.py
+++ screenRecorder.py
@@ -12,9 +12,11 @@ GitHub link: https://github.com/PaleTurtle/CST-205-Project-2
 
 import sys
 import subprocess
+import pyscreenshot as ImageGrab
 from PyQt5.QtWidgets import (QWidget,QMainWindow,QAction,QApplication,qApp,QHBoxLayout,
                              QVBoxLayout,QPushButton)
 from PyQt5.QtGui import QPixmap, QIcon
+
 
 class Window(QMainWindow):
 
@@ -64,37 +66,31 @@ class Window(QMainWindow):
         text = self.textEdit.toPlainText()
         file.write(text)
         file.close()
-  
         
     #Record click
     def rClick(self):
+        filename="newfile"
+        counter=0
+        time=20
+
+        for x in range(1,10):
+                counter = counter + 1   
+                if(counter<=10):
+                        ImageGrab.grab().save("screenshot00"+(str(x))+".png")
+                elif(counter>10 and counter<=100):
+                        ImageGrab.grab().save("screenshot0"+(str(x))+".png")
+                elif(counter>100 and counter<1000):
+                        ImageGrab.grab().save("screenshot"+(str(x))+".png")
+
+        subprocess.call('ffmpeg -framerate 1/5 -f image2 -i screenshot%03d.png -c:v libx264 -vf fps=1 fvideo.ts', shell="TRUE")
         
-        #import the subprocess for oprations with FFmpeg and the command line
-        #link to GitHub: https://github.com/PaleTurtle/CST-205-Project-2/
-        #user chooses the filename for the output file
-        filename=input('Please enter the filename you want:')
-        #user chooses the amount of time he wants to record
-        time=input('How long do you want to record(seconds)?')
-        #callCommand function:call the FFmpeg command in python, even though it is made for command line
-        #got some help from stackoverflow:https://stackoverflow.com/questions/7656308/python-ffmpeg-command-line-issues
-        #input: the FFmpeg command
         def callCommand(command):
             #open the command in the subprocess and split it into the parts that should be executed
             subprocess.Popen(command.split(' '))
 
         #use the command for the FFmpeg function
-        callCommand('ffmpeg -f alsa -ac 2 -ar 44100 -ss 00:00:00 -t {} -i hw:0,1 {}.wav'.format(time,filename))
-            #'ffmpeg: execute FFmpeg
-            #-f alsa:format of capturing is ALSA
-            #-ac 2:2 audio channels: stereo recording
-            #-ar 44100: audio rate: 44100 Hz (usual rate for audio recording) 
-            #-ss 00:00:00: start from the beginning
-            #-t {}: record for the time the user entered
-            #-i: input: all the stuff from before is used as input
-            #hw:0,1: select channel for microphone
-            #{}.wav': filename the user entered is used for the output file
-            #.format(time,filename)):input from user is used to fill the gaps in command
-        
+        callCommand('ffmpeg -f alsa -ac 2 -ar 44100 -ss 00:00:00 -t 20 -i hw:0,1 project2finaltest.mp3')
+        callCommand('ffmpeg -i fvideo.ts -i project2finaltest.mp3 -c:v copy -c:a copy finalprojecttestrun.mp4')
 
 if __name__ == '__main__':
 
