dd374b0030ff2b7b292a3a5ea7991a56f9742ae2
VixMix91 <edone@csumb.edu>
2016-10-14 21:54:24 -0700

    Update video.py

--- video.py
+++ video.py
@@ -1,17 +1,23 @@
-from PIL import ImageGrab
-from PIL import Image
-from ffmpy import FFmpeg
+from PIL import ImageGrab #Allows us to use ImageGrab = Screenshot
 
-Image_VideoList = []
+import subprocess
 
-for x in range(1,5):
-    ImageGrab.grab().save((str(x)) + "screenshot.jpg", "JPEG")
+#run screenshots
+counter = 0
+for x in range(1,time
+):
+	counter = counter + 1
+	if(counter<=10):
+		ImageGrab.grab().save("screenshot00" + str(x) + ".png")
+	elif(counter>10 and counter<100):
+		ImageGrab.grab().save("screenshot0" + str(x) + ".png")
+	elif(counter>=100 and counter<1000):
+		ImageGrab.grab().save("screenshot" + str(x) + ".png")
+	
 
-for imageNumber in range(1,5):
-    Image_VideoList.append((str(imageNumber)) + "screenshot.jpg")
 
-# print (Image_VideoList)
+#convert to video using ffmpeg
+subprocess.call('ffmpeg -r 1 -f image2 -i /Desktop/Project205/screenshot%03d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p ssvideo.ts')
 
-video = FFmpeg(
-        inputs={'input.ts' = ImageVideoList,
-        outputs ={'output.mp4':
+# to combine audio and video using ffmpeg
+# ffmpeg -i (video file) -i (audio file) -c:v copy -c:a copy (outputname).mp4))
